298ec2c22f513e76f5bd4f32d105647149765b0e
gottabpro <sj931293@gmail.com>
2021-07-16 00:25:51 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-function compute()
-{
-    var principal = document.getElementById("principal").value;
-    if(principal <= 0){
-        alert("Enter a positive number")
-        setTimeout(function(){
-                document.getElementById("principal").focus();
-            }, 1);
-    }
-    var rate = document.getElementById("rate").value;
-    var years = document.getElementById("years").value;
-    var interest = principal * years * rate/100;
-    var year = new Date().getFullYear()+parseInt(years);
-    var amount = Number(principal + interest);
-    document.getElementById("result").innerText="If you deposit \<mark>"+principal+
-    "\<\mark>,\<br\>at an interest rate of \<mark>"+rate+"\<\mark>%\<br\>You will receive an amount of \<mark>"+
-    amount+"\<\mark>,\<br\>in the year \<mark>"+year+"\<\mark>\<br\>"
+function compute() {
+  var principal = document.getElementById("principal").value;
+  if (principal <= 0) {
+    alert("Enter a positive number");
+    setTimeout(function () {
+      document.getElementById("principal").focus();
+    }, 1);
+  }
+  var rate = document.getElementById("rate").value;
+  var years = document.getElementById("years").value;
+  var interest = (principal * years * rate) / 100;
+  var year = new Date().getFullYear() + parseInt(years);
+  var num1 = Number(principal);
+  var num2 = Number(interest);
+  var amount = num1 + num2;
+  console.log(amount);
+  document.getElementById("result").innerText =
+    "If you deposit <mark>" +
+    principal +
+    "</mark>,<br>at an interest rate of <mark>" +
+    rate +
+    "</mark>%<br>You will receive an amount of <mark>" +
+    amount +
+    "</mark>,<br>in the year <mark>" +
+    year +
+    "</mark>\\<br>";
 }
 
-function updateRate(){
-    var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+function updateRate() {
+  var rateval = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateval;
 }
-        
