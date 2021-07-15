6ff4ea0de53e12a1c805dee48ebd0c64e3c420f3
Sj-001 <sj931293@gmail.com>
2021-07-15 16:10:10 +0000

    Initial Commit

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if(principal <= 0){
+        alert("Enter a positive number")
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate/100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var amount = principal + interest;
+    document.getElementById("result").innerText="If you deposit \<mark>"+principal+"\<\mark>,\<br\>at an interest rate of \<mark>"+rate+"\<\mark>%\<br\>You will receive an amount of \<mark>"+amount+"\<\mark>,\<br\>in the year \<mark>"+year+"\<\mark>\<br\>"
+}
+
+function updateRate(){
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
 }
         
